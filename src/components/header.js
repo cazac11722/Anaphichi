cb4edf0b11c674834f1df521783fdbf5f2b9c71f
박상민 <cazac@bagsangmin-ui-MacBookPro.local>
2025-01-13 23:05:35 +0900

    수정

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,32 @@
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 const Header = () => {
+    useEffect(() => {
+        const handleMenuToggle = () => {
+            document.body.classList.toggle("not_scroll");
+            document.querySelector(".ham_btn").classList.toggle("open");
+            document.querySelector("nav").classList.toggle("open");
+        };
+
+        const hamBtn = document.querySelector(".ham_btn");
+        hamBtn.addEventListener("click", handleMenuToggle);
+
+        return () => {
+            hamBtn.removeEventListener("click", handleMenuToggle);
+        };
+    }, []);
+
     return (
         <>
             <div id="m_menu" className="deth-1">
                 <ul>
-                    <li><a href="index.html">HOME</a></li>
-                    <li><a href="anapich.html">ABOUT</a></li>
-                    <li><a href="class.html">CLASS</a></li>
-                    <li><a href="trainer.html">TRAINER</a></li>
-                    <li><a href="facility.html">FACILITY</a></li>
-                    <li><a href="">AFFILIATED CONCERN</a></li>
+                    <li><Link to="/">HOME</Link></li>
+                    <li><Link to="/anapich">ABOUT</Link></li>
+                    <li><Link to="/class">CLASS</Link></li>
+                    <li><Link to="/trainer">TRAINER</Link></li>
+                    <li><Link to="/facility">FACILITY</Link></li>
+                    <li><Link to="/affiliated">AFFILIATED</Link></li>
                 </ul>
                 <button type="button" className="ham_btn">
                     <i className="far fa-times" aria-hidden="true"></i>
@@ -35,6 +51,6 @@ const Header = () => {
             </header>
         </>
     );
-}
+};
 
 export default Header;
